0a8381a1e52aa266780a45d971e34b03b30982e6
Alnikov Aleksandr <web3aleksandr@gmail.com>
2024-08-26 13:00:32 +0700

    ***

---
+++ src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader.js';
+
+class AppLoader extends Loader {
+    constructor() {
+        super(process.env.API_URL, {
+            apiKey: process.env.API_KEY,
+        });
+    }
+}
+
+export default AppLoader;
